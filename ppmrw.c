1c0e9c2764e8a80a2342da459153171d28b7a413
Colter Hooker <ceh248@nau.edu>
2017-09-14 16:43:30 -0700

    Last push: remove what I had done for the fact it had noe value to the project. Updated now to where P6 is working

--- ppmrw.c
+++ ppmrw.c
@@ -24,40 +24,27 @@ typedef struct ppmImage
 } ppmImage;
 
 // function decoration
-int writeP3(char *inputFile, char *outputFile, ppmImage inputImage, FILE *inFile, FILE *outFile);
-int writeP6(char *inputFile, char *outputFile, ppmImage inputImage, FILE *inFile, FILE *outFile);
-int readFile(int ppmFormat, char *inputFile,char *outputFile, ppmImage inputImage, FILE *inFile, FILE *outFile);
+int writeP3(char *inputFile, char *outputFile, ppmImage inputImage);
+int writeP6(char *inputFile, char *outputFile, ppmImage inputImage);
+int readFile(int ppmFormat, char *inputFile,char *outputFile, ppmImage inputImage);
 
 //write to a file in P3 format from a P6 format
-int writeP3(char *inputFile, char *outputFile, ppmImage inputImage, FILE *inFile, FILE *outFile)
+int writeP3(char *inputFile, char *outputFile, ppmImage inputImage)
 {
-    //
-    char buffer[256], *fileHeader;
     FILE* write = fopen(outputFile,"w");
     inputImage.data = (ppmRGBpixel *)malloc(sizeof(ppmRGBpixel) * inputImage.width * inputImage.heigth);
     fprintf(write, "P3\n%d %d\n%d\n",inputImage.width, inputImage.heigth, inputImage.maxColorInten);
+
     fclose(write);
     printf("Wrote to %s in P3 from P%c\n", outputFile, inputImage.magicNumber);
     return 0;
 }
 //write to a file in P6 format from a P3 format
-int writeP6(char *inputFile, char *outputFile, ppmImage inputImage, FILE *inFile, FILE *outFile)
+int writeP6(char *inputFile, char *outputFile, ppmImage inputImage)
 {
-    //
-    char buffer[SIZE], *fileHeader;
-    fileHeader = fgets(buffer,SIZE, inFile);
     FILE* write = fopen(outputFile,"w");
-    inputImage.data = (ppmRGBpixel *)malloc(sizeof(ppmRGBpixel) * inputImage.width * inputImage.heigth);
     fprintf(write, "P6\n%d %d\n%d\n",inputImage.width, inputImage.heigth, inputImage.maxColorInten);
-    //read the ascii file, convert the characters to int, and store it in our buffer
-    /*
-    for (int wtxt=0; wtxt <inputImage.width * inputImage.heigth; wtxt++)
-    {
-        //
-        int curVal;
-
-    }
-    */
+    fwrite(inputImage.data, 3 * inputImage.width, inputImage.heigth, write);
     fclose(write);
     printf("Wrote to %s in P6 from P%c\n", outputFile, inputImage.magicNumber);
     return 0;
@@ -65,7 +52,7 @@ int writeP6(char *inputFile, char *outputFile, ppmImage inputImage, FILE *inFile
 
 // read in the input file to be stored to memory and then determine
 // what writing porcess to do
-int readFile(int ppmFormat, char *inputFile, char *outputFile, ppmImage inputImage, FILE *inFile, FILE *outFile)
+int readFile(int ppmFormat, char *inputFile, char *outputFile, ppmImage inputImage)
 {
     // temp Variable for the image
     int tempMagicNumber, tempWidth,tempHeight,tempMaxColorValue, checkwh;
@@ -121,18 +108,38 @@ int readFile(int ppmFormat, char *inputFile, char *outputFile, ppmImage inputIma
         fprintf(stderr, "Error: Incorrect file color intensity, must be 1 to 255\n");
     }
     inputImage.maxColorInten = tempMaxColorValue;
-
+    ppmVal = fgetc(read);
+    while(ppmVal != '\n')
+    {
+        if (ppmVal == '#')
+        {
+            ppmVal = fgetc(read);
+        }
+        ppmVal = fgetc(read);
+    }
+    inputImage.data = (ppmRGBpixel *)malloc(sizeof(ppmRGBpixel) * inputImage.width * inputImage.heigth);
+    for (int wtxt=0; wtxt <inputImage.width * inputImage.heigth; wtxt++)
+    {
+        //
+        int curVal;
+        fscanf(read,"%d",&curVal);
+        inputImage.data[wtxt].r = curVal;
+        fscanf(read,"%d",&curVal);
+        inputImage.data[wtxt].g = curVal;
+        fscanf(read,"%d",&curVal);
+        inputImage.data[wtxt].b = curVal;
+    }
     fclose(read);
     /*
      * to determine where to write the file too
      */
     if (ppmFormat == 3)
     {
-        writeP3(inputFile, outputFile, inputImage, inFile, outFile);
+        writeP3(inputFile, outputFile, inputImage);
     }
     if (ppmFormat == 6)
     {
-        writeP6(inputFile, outputFile, inputImage, inFile, outFile);
+        writeP6(inputFile, outputFile, inputImage);
     }
     return 0;
 }
@@ -145,8 +152,6 @@ int main(int argc, char *argv[])
     int ppmFormat = atoi(argv[1]); // need fixing
     char *inputFile = argv[2];
     char *outputFile = argv[3];
-    FILE *inFile = fopen(argv[2], "rb");
-    FILE *outFile = fopen(argv[3], "wb");
 
     // Variable to used to see if the file exist
     FILE *fileExist;
@@ -183,7 +188,7 @@ int main(int argc, char *argv[])
     // read the file into the ppm Image struct
     ppmImage *inputImage = malloc(sizeof(ppmImage));
     // go to the read file
-    readFile(ppmFormat,inputFile,outputFile, *inputImage,inFile,outFile);
+    readFile(ppmFormat,inputFile,outputFile, *inputImage);
     fclose(fileExist);
     return 0;
 }
